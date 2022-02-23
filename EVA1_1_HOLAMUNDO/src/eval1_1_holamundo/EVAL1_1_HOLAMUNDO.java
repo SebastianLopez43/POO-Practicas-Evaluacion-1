8737cfde7fab9403847ed95bb7ba2b7712caa0a3
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-02-23 10:55:01 -0600

    Práctica 1_HolaMundo

---
+++ EVA1_1_HOLAMUNDO/src/eval1_1_holamundo/EVAL1_1_HOLAMUNDO.java
@@ -0,0 +1,24 @@
+package eval1_1_holamundo;
+
+/**
+ @author José Sebastian López Ibarra
+ */
+
+public class EVAL1_1_HOLAMUNDO { // Cuerpo de una clase / Declaraciones
+    /* 
+    System.out.printIn("Hello World!");
+    Falta de identificador: Inicio de tipo ilegal
+    */
+    
+    // Declarar variables que usaremos a lo largo del programa
+    int x;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // Función / Código a trabajar
+        // Imprimir en consola
+        System.out.println("Hello World!!");
+    }
+    
+}
