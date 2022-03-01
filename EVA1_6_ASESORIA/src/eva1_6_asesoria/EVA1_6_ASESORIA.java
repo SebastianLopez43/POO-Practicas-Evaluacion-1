6243edba3972ed3db4ee9616bb3af8190491af7d
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-02-28 23:21:13 -0600

    Práctica 6_Asesoría

---
+++ EVA1_6_ASESORIA/src/eva1_6_asesoria/EVA1_6_ASESORIA.java
@@ -0,0 +1,30 @@
+package eva1_6_asesoria;
+
+/**
+ *
+ * @author José Sebastian López Ibarra
+ * Friday February 25 2022
+ */
+
+public class EVA1_6_ASESORIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        int value1 = 10;
+        System.out.println("Value 1: " + value1);
+        int value2 = Integer.MAX_VALUE; // Valor entero más grande que ocupa un entero
+        System.out.println("Value 2: " + value2);
+        value2 += 1;
+        System.out.println("-- Value 2 + 1 --");
+        System.out.println("Value 2: " + value2);
+        
+        /*
+        111111111 + 1 = 10000000
+        1 + 1 = 10
+        */
+    }
+    
+}
