b2f6b5e2ac780ac747a8a0ebd7291053b62d48fc
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-03-07 10:56:41 -0600

    Práctica 11_Clases

---
+++ EVA1_11_CLASES/src/eva1_11_clases/EVA1_11_CLASES.java
@@ -0,0 +1,18 @@
+package eva1_11_clases;
+
+/**
+ * @author José Sebastian López Ibarra
+ * Thursday March 03 2022
+ */
+
+public class EVA1_11_CLASES {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
